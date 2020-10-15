7f455b6c8bf864dc25602d4fbdba369481648eb2
Antok Nurwicaksono <72114629+antokwicaksonojoyo@users.noreply.github.com>
2020-10-15 19:11:20 +0700

    Rangkaian String
    Menambah data picker
    Menambah rangkaian string
    Menambah tombol reset

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-int quantity = 2;
+int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +24,30 @@ int quantity = 2;
     }
 
     public void decrement (View view) {
-       if (quantity = quantity - 1){
-           quantity = 0;
-       };
+        quantity = quantity - 1;
+
         display(quantity);
     }
 
-
     public void submitOrder (View view) {
-
-        displayPrice (quantity * 5000);
+        int price = quantity * 5000;
+        String priceMessage = "Total Pembelian Rp. " + price;
+        priceMessage = priceMessage + "\nMatursalangkung!";
+        displayMessage (priceMessage);
     }
+
+    public void submitReset (View view) {
+        if (quantity == quantity){
+            quantity = 0;
+        };
+        display(quantity);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
     private void display (int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText ("" + number);
